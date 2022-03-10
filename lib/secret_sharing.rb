ebd990b0eedeb2c9bc2758bf9ddb3b9cc35076f6
David William Silva <contact@davidwsilva.com>
2022-03-10 15:03:00 -0700

    Initial commit

---
+++ lib/secret_sharing.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "secret_sharing/version"
+
+module SecretSharing
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/secret_sharing/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SecretSharing
+  VERSION = "0.1.0"
+end
